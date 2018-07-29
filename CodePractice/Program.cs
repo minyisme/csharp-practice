eac2e48c7d6bb56fd1a85313f52b737fc3d75b3a
Min Yang (InConsulting Inc.) <v-miyan@microsoft.com>
2018-07-29 12:10:22 -0700

    added enumeration and switch

--- CodePractice/Program.cs
+++ CodePractice/Program.cs
@@ -8,6 +8,9 @@ namespace CodePractice
 {
     class Program
     {
+        // enumerations used for switch later
+        public enum MonthsOfYear { January=1, February=2, March=3, April=4, May=5, June=6 };
+
         static void Main(string[] args)
         {
 
@@ -86,12 +89,48 @@ namespace CodePractice
 
             // arrays and foreach loops
 
-            string[] numbers = new string[6] { "a", "b", "c", "d", "e", "f" };
-            foreach (string letter in numbers)
+            string[] letters = new string[6] { "a", "b", "c", "d", "e", "f" };
+            foreach (string letter in letters)
             {
                 Console.WriteLine($"The letter is now {letter}.");
             }
+
+            letters[3] = "g";
+            foreach (string l in letters)
+            {
+                Console.WriteLine($"The letter is now {l}.");
+            }
+            Console.ReadKey();
+
+            // enumeration and switch
+
+            Console.WriteLine("Which month?");
+            int today = Convert.ToInt32(Console.ReadLine());
+            switch (today)
+            {
+                case 1:
+                    Console.WriteLine("It's January");
+                    break;
+                case 2:
+                    Console.WriteLine("It's February");
+                    break;
+                case 3:
+                    Console.WriteLine("It's March");
+                    break;
+                case 4:
+                    Console.WriteLine("It's April");
+                    break;
+                case 5:
+                    Console.WriteLine("It's May");
+                    break;
+                case 6:
+                    Console.WriteLine("It's June");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid month");
+                    break;
+            }
             Console.ReadKey();
         }
-    }
+    }       
 }
